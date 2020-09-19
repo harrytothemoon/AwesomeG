a0efb92e0a5df9e519e7053b9330163e86020392
Harry <harry811016@gmail.com>
2020-09-20 01:48:05 +0800

    安裝socketio套件/socketio初始化設定/修正putUser type問題

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require('express')
+const http = require("http");
+const socketIO = require("socket.io");
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const cors = require('cors')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketIO(server)
 const port = process.env.PORT || 3000
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+  io.emit("chat message", "hello");
+
+  socket.on("private message", (message) => {
+    socket.emit("chat message", message);
+  });
+
+  socket.on("global message", (message) => {
+    io.emit("chat message", message);
+  });
+
+  socket.on("broadcast message", (message) => {
+    socket.broadcast.emit("chat message", message);
+  });
+});
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -15,7 +36,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(methodOverride('_method'))
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
