af3454bfb6b29a4f2c68792e1f2117a2a556de71
Harry <harry811016@gmail.com>
2020-09-30 09:51:58 +0800

    新增Notification API測試

--- test/requests/Answer.spec.js
+++ test/requests/Answer.spec.js
@@ -7,107 +7,105 @@ const db = require('../../models')
 const { Answer, User, Question } = db
 
 describe('# Answer request', () => {
-  context('# After sign in', () => {
-    let token = ''     // for saving sign in token
-    const testTeacher = {
-      name: 'test',
-      email: 'test@example.com',
-      password: 'test',
-      role: 'teacher'
-    }
-    const testQuestion = {
-      description: 'test',
-      UserId: '2',
-    }
-    before(async () => {
-      await User.destroy({ where: {}, truncate: true })
-      await Answer.destroy({ where: {}, truncate: true })
-      await Question.destroy({ where: {}, truncate: true })
-      // create a test user
-      const salt = await bcrypt.genSalt(10)
-      const hash = await bcrypt.hash(testTeacher.password, salt)
-      await User.create({
-        name: testTeacher.name,
+  let token = ''     // for saving sign in token
+  const testTeacher = {
+    name: 'test',
+    email: 'test@example.com',
+    password: 'test',
+    role: 'teacher'
+  }
+  const testQuestion = {
+    description: 'test',
+    UserId: '2',
+  }
+  before(async () => {
+    await User.destroy({ where: {}, truncate: true })
+    await Answer.destroy({ where: {}, truncate: true })
+    await Question.destroy({ where: {}, truncate: true })
+    // create a test user
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(testTeacher.password, salt)
+    await User.create({
+      name: testTeacher.name,
+      email: testTeacher.email,
+      password: hash,
+      role: testTeacher.role
+    })
+    await Question.create({
+      description: testQuestion.description,
+      UserId: testQuestion.UserId,
+    })
+    // sign in as test user   
+    await fetch(`${HOST}:${INTERNAL_PORT}/api/signin`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
         email: testTeacher.email,
-        password: hash,
-        role: testTeacher.role
+        password: testTeacher.password
       })
-      await Question.create({
-        description: testQuestion.description,
-        UserId: testQuestion.UserId,
-      })
-      // sign in as test user   
-      await fetch(`${HOST}:${INTERNAL_PORT}/api/signin`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          email: testTeacher.email,
-          password: testTeacher.password
-        })
-      })
-        .then(res => res.json())
-        .then(res => {
-          token = res.token
-        })
     })
+      .then(res => res.json())
+      .then(res => {
+        token = res.token
+      })
+  })
 
-    it('POST /api/teacher/answer', async () => {
-      await fetch(`${HOST}:${INTERNAL_PORT}/api/teacher/answer`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer ' + token,
-        },
-        body: JSON.stringify({ questionId: 1 })
+  it('POST /api/teacher/answer', async () => {
+    await fetch(`${HOST}:${INTERNAL_PORT}/api/teacher/answer`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token,
+      },
+      body: JSON.stringify({ questionId: 1 })
+    })
+      .then(res => {
+        assert.strictEqual(res.status, 200)
+        return res.json()
       })
-        .then(res => {
-          assert.strictEqual(res.status, 200)
-          return res.json()
-        })
-        .then(res => {
-          assert.strictEqual(res.message, 'Get the Question!')
-        })
-    })
-
-    it('PUT /api/teacher/answer', async () => {
-      await fetch(`${HOST}:${INTERNAL_PORT}/api/teacher/answer`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer ' + token,
-        },
-        body: JSON.stringify({ questionId: 1, answer: 'test' })
+      .then(res => {
+        assert.strictEqual(res.message, 'Get the Question!')
       })
-        .then(res => {
-          assert.strictEqual(res.status, 200)
-          return res.json()
-        })
-        .then(res => {
-          assert.strictEqual(res.message, 'The answer has been sent!')
-        })
-    })
+  })
 
-    it('GET /api/teacher/answers', async () => {
-      await fetch(`${HOST}:${INTERNAL_PORT}/api/teacher/answers`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer ' + token,
-        }
+  it('PUT /api/teacher/answer', async () => {
+    await fetch(`${HOST}:${INTERNAL_PORT}/api/teacher/answer`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token,
+      },
+      body: JSON.stringify({ questionId: 1, answer: 'test' })
+    })
+      .then(res => {
+        assert.strictEqual(res.status, 200)
+        return res.json()
       })
-        .then(res => {
-          assert.strictEqual(res.status, 200)
-          return res.json()
-        }).then(res => {
-          assert.strictEqual(res.answers[0].UserId, 1)
-        })
-    })
+      .then(res => {
+        assert.strictEqual(res.message, 'The answer has been sent!')
+      })
+  })
 
-    after(async () => {
-      // remove the test user and url
-      await User.destroy({ where: {}, truncate: true })
-      await Answer.destroy({ where: {}, truncate: true })
-      await Question.destroy({ where: {}, truncate: true })
+  it('GET /api/teacher/answers', async () => {
+    await fetch(`${HOST}:${INTERNAL_PORT}/api/teacher/answers`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token,
+      }
     })
+      .then(res => {
+        assert.strictEqual(res.status, 200)
+        return res.json()
+      }).then(res => {
+        assert.strictEqual(res.answers[0].UserId, 1)
+      })
+  })
+
+  after(async () => {
+    // remove the test user and url
+    await User.destroy({ where: {}, truncate: true })
+    await Answer.destroy({ where: {}, truncate: true })
+    await Question.destroy({ where: {}, truncate: true })
   })
 })
---
+++ test/requests/Notification.spec.js
@@ -0,0 +1,73 @@
+const assert = require('assert')
+const bcrypt = require('bcryptjs')
+const fetch = require('node-fetch')
+const HOST = process.env.HOST || 'http://localhost'
+const INTERNAL_PORT = 3000
+const db = require('../../models')
+const { User, Notification } = db
+
+describe('# Notification request', () => {
+  let token = ''     // for saving sign in token
+  const testStudent = {
+    name: 'test',
+    email: 'test@example.com',
+    password: 'test',
+    role: 'student'
+  }
+  const testNotification = {
+    msg: 'test',
+    UserId: '1',
+  }
+  before(async () => {
+    await User.destroy({ where: {}, truncate: true })
+    await Notification.destroy({ where: {}, truncate: true })
+    // create a test user
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(testStudent.password, salt)
+    await User.create({
+      name: testStudent.name,
+      email: testStudent.email,
+      password: hash,
+      role: testStudent.role
+    })
+    await Notification.create({
+      msg: testNotification.msg,
+      UserId: testNotification.UserId,
+    })
+    // sign in as test user   
+    await fetch(`${HOST}:${INTERNAL_PORT}/api/signin`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        email: testStudent.email,
+        password: testStudent.password
+      })
+    })
+      .then(res => res.json())
+      .then(res => {
+        token = res.token
+      })
+  })
+
+  it('GET /api/notifications', async () => {
+    await fetch(`${HOST}:${INTERNAL_PORT}/api/notifications`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token,
+      }
+    })
+      .then(res => {
+        assert.strictEqual(res.status, 200)
+        return res.json()
+      }).then(res => {
+        assert.strictEqual(res.notifications[0].msg, 'test')
+      })
+  })
+
+  after(async () => {
+    // remove the test user and url
+    await User.destroy({ where: {}, truncate: true })
+    await Notification.destroy({ where: {}, truncate: true })
+  })
+})
